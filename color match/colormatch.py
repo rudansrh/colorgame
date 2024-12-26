d9803c95be6da3657764c39ec5a39ac1a979153e
김정률 <127955804+rudansrh@users.noreply.github.com>
2024-12-26 23:51:59 +0900

    111

--- color match/colormatch.py
+++ color match/colormatch.py
@@ -1,7 +1,11 @@
 import pygame
 import sys
 import time
+import random
 pygame.init()
+
+score = 0
+e_size = 50
 
 #화면 크기 설정
 screen_width = 800
@@ -9,15 +13,18 @@ screen_height = 550
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Color Match")
 
-red_cir = pygame.image.load("이미지 저장\\red.png")
-red_cir = pygame.transform.scale(red_cir,(30,30))
-green_cir = pygame.image.load("이미지 저장\\green.png")
-green_cir = pygame.transform.scale(green_cir,(30,30))
-blue_cir = pygame.image.load("이미지 저장\\blue.png")
-blue_cir = pygame.transform.scale(blue_cir,(30,30))
-triangle = pygame.image.load("이미지 저장\\TRIANGLE.png")
-triangle = pygame.transform.scale(triangle,(80,80))
-myFont = pygame.font.Font("CookieRun Regular.ttf", 30)
+player1 = pygame.image.load("이미지 저장\\player1.png")
+player1 = pygame.transform.scale(player1,(130,100))
+player2 = pygame.image.load("이미지 저장\\player2.png")
+player2 = pygame.transform.scale(player2,(130,100))
+
+r_cir = pygame.image.load("이미지 저장\\red.png")
+r_cir = pygame.transform.scale(r_cir,(e_size,e_size))
+b_cir = pygame.image.load("이미지 저장\\blue.png")
+b_cir = pygame.transform.scale(b_cir,(e_size,e_size))
+circles = [r_cir, b_cir]
+
+myFont = pygame.font.Font("CookieRun Regular.ttf", 40)
 
 #색 설정
 black = (0,0,0)
@@ -28,32 +35,72 @@ blue = (0, 0, 255)
 yellow = (255, 255, 0)
 bgcolor = (0,0,0)
 
-enemysize = (1000,30)
-circlepos = [(400-15,250-15),(425-15,290-15),(375-15,290-15),(0,0)]
+Lrect = pygame.Rect(345,230,50,50)
+Rrect = pygame.Rect(405,230,50,50)
+
+enemylist = [] #[pos, dir(+1,-1), color]
+
+speed = 4
+cooltime = 1.3
+mincooltime = 0.5
 clock = pygame.time.Clock()
+startTime = 0
+playerstate = 0 #0: 왼-빨, 오른-파  1: 왼-파, 오른-빨
+
 running = True
 while running:
+    clock.tick(60)
     screen.fill(bgcolor)
-    screen.blit(triangle, (360,230))
-    screen.blit(red_cir,circlepos[0])
-    screen.blit(blue_cir,circlepos[1])
-    screen.blit(green_cir,circlepos[2])
+    if playerstate == 0:
+        screen.blit(player1, (335, 200))
+    else:
+        screen.blit(player2, (335, 200))
+    scoreText = myFont.render(f"{score}", True,white)
+    screen.blit(scoreText, (390, 10))
+    
+    dell = -1
+    for i in range(len(enemylist)):
+        enemylist[i][0][0] += enemylist[i][1]*speed
+        e_rect = pygame.Rect(int(enemylist[i][0][0]),int(enemylist[i][0][1]),e_size,e_size)
+        screen.blit(circles[enemylist[i][2]], tuple(enemylist[i][0]))
+        if e_rect.colliderect(Lrect) or e_rect.colliderect(Rrect):
+            if playerstate == 0:
+                if (enemylist[i][1] == 1 and enemylist[i][2] == 0) or (enemylist[i][1] == -1 and enemylist[i][2] == 1):
+                    score += 10
+                else:
+                    running = False
+            else:
+                if (enemylist[i][1] == 1 and enemylist[i][2] == 1) or (enemylist[i][1] == -1 and enemylist[i][2] == 0):
+                    score += 10
+                else:
+                    running = False
+            dell = i
+    if dell != -1:
+        del(enemylist[dell])
+        
+    if (pygame.time.get_ticks() - startTime)/1000 > cooltime:
+        startTime = pygame.time.get_ticks()
+        if cooltime > mincooltime:
+            cooltime -= 0.01
+        a = random.randint(1,2)
+        if a == 1:
+            s_pos = [-90,220]
+            direc = 1
+            c = random.randint(0,1) #0:빨, 1:파
+            enemylist.append([s_pos, direc, c])
+        else:
+            s_pos = [820, 220]
+            direc = -1
+            c = random.randint(0,1) #0:빨, 1:파
+            e_image = circles[c]
+            enemylist.append([s_pos, direc, c])
+            
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                circlepos[3] = circlepos[0]
-                circlepos[0] = circlepos[1]
-                circlepos[1] = circlepos[2]
-                circlepos[2] = circlepos[3]
-            if event.key == pygame.K_LEFT:
-                circlepos[3] = circlepos[2]
-                circlepos[2] = circlepos[1]
-                circlepos[1] = circlepos[0]
-                circlepos[0] = circlepos[3]
-    
+            playerstate = 1-playerstate
     pygame.display.flip()
