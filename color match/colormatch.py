6ec9d0f3d8eae75741bef577e2cc94697932ef9c
김정률 <127955804+rudansrh@users.noreply.github.com>
2024-12-26 20:57:58 +0900

    이미지 변경

--- color match/colormatch.py
+++ color match/colormatch.py
@@ -3,12 +3,18 @@ import sys
 import time
 pygame.init()
 
-#화면 크기 설정정
+#화면 크기 설정
 screen_width = 800
 screen_height = 550
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Color Match")
 
+red_cir = pygame.image.load("이미지 저장\\red.png")
+red_cir = pygame.transform.scale(red_cir,(30,30))
+green_cir = pygame.image.load("이미지 저장\\green.png")
+green_cir = pygame.transform.scale(green_cir,(30,30))
+blue_cir = pygame.image.load("이미지 저장\\blue.png")
+blue_cir = pygame.transform.scale(blue_cir,(30,30))
 triangle = pygame.image.load("이미지 저장\\TRIANGLE.png")
 triangle = pygame.transform.scale(triangle,(80,80))
 myFont = pygame.font.Font("CookieRun Regular.ttf", 30)
@@ -23,15 +29,15 @@ yellow = (255, 255, 0)
 bgcolor = (0,0,0)
 
 enemysize = (1000,30)
-circlepos = [(400,250),(425,290),(375,290),(0,0)]
+circlepos = [(400-15,250-15),(425-15,290-15),(375-15,290-15),(0,0)]
 clock = pygame.time.Clock()
 running = True
 while running:
     screen.fill(bgcolor)
     screen.blit(triangle, (360,230))
-    circle1 = pygame.draw.circle(screen, red, circlepos[0], 10)
-    circle2 = pygame.draw.circle(screen, green, circlepos[1], 10)
-    circle3 = pygame.draw.circle(screen, yellow, circlepos[2], 10)
+    screen.blit(red_cir,circlepos[0])
+    screen.blit(blue_cir,circlepos[1])
+    screen.blit(green_cir,circlepos[2])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
